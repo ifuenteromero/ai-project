dd45e252ec06c5aac1d629b1b92ea53d139d0861
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-11-17 07:11:34 +0100

    Formatting code with prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+	const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => {
-        setMessage(data.message);
-      });
-  }, []);
+	useEffect(() => {
+		fetch('/api/hello')
+			.then(res => res.json())
+			.then(data => {
+				setMessage(data.message);
+			});
+	}, []);
 
-  return (
-    <div className="p-4 flex flex-col gap-4">
-      <span className="font-bold text-4xl text-cyan-600">{message}</span>
-      <Button className="max-w-fit">Click me</Button>
-    </div>
-  );
+	return (
+		<div className='p-4 flex flex-col gap-4'>
+			<span className='font-bold text-4xl text-cyan-600'>{message}</span>
+			<Button className='max-w-fit'>Click me</Button>
+		</div>
+	);
 }
 
 export default App;
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,19 +1,19 @@
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
-  },
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		proxy: {
+			'/api': 'http://localhost:3000',
+		},
+	},
 });
