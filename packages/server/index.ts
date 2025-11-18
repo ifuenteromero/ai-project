9e690268729eaff17c0ca23786668186bf0ef218
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-11-18 07:31:43 +0100

    Building the Chat API

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,18 @@
+import dotenv from 'dotenv';
 import type { Request, Response } from 'express';
 import express from 'express';
-import dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 dotenv.config();
 
+const client = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+// middleware para parsear JSON.
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -13,6 +21,20 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
 	res.json({ message: 'Hello from the API!' });
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+	const { prompt } = req.body;
+	const response = await client.responses.create({
+		model: 'gpt-4o-mini',
+		input: prompt,
+		temperature: 0.2,
+		max_output_tokens: 100,
+	});
+
+	return res.json({
+		message: response.output_text,
+	});
 });
 
 app.listen(port, () => {
